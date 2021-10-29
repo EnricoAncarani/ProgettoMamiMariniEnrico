72462fd9bca6c80430a13f02817450b3c71bab4a
Elia Mami <ELIA.MAMI@local.ittcesena.it>
2021-10-29 09:24:50 +0200

    V1: classe cd

---
+++ ProgettoCoppie/CD.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoCoppie
+{
+    class CD
+    {
+        private string _autore;
+        private string _titolo;
+        private List<Brano> _brani;
+        public string Titolo
+        {
+            get { return _titolo; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Titolo non valido.");
+                _titolo = value;
+            }
+        }
+        public string Autore
+        {
+            get { return _autore; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Autore non valido.");
+                _autore = value;
+            }
+        }
+        public List<Brano> Brani
+        {
+            get { return _brani; }
+            set { _brani = value; }
+        }
+        public CD(string titolo, string autore)
+        {
+            _titolo = titolo;
+            _autore = autore;
+            _brani = new List<Brano>();
+        }
+
+        public addBrano(Brano b)
+        {
+            _brani.Add(b);
+        }
+        
+        public int DurataCD()
+        {
+            int durataCD = 0;
+            foreach(Brano b in _brani)
+            {
+                durataCD = durataCD + b.Durata;
+            }
+        }
+        public override string ToString()
+        {
+            string listaTitoli = "";
+            foreach(Brano b in _brani)
+            {
+                listaTitoli = b.Titolo + " ";
+            }
+            return listaTitoli;
+        }
+    }
+}
