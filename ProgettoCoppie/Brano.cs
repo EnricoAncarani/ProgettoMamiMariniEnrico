1b240b794874a4cb5b3cd41cb1b54561644830f9
Andrea Marini <ANDREA.MARINI@local.ittcesena.it>
2021-10-29 09:24:01 +0200

    V3 aggiunta costruttore

--- ProgettoCoppie/Brano.cs
+++ ProgettoCoppie/Brano.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace ProgettoCoppie
 {
-    class Class1
+    class Brano
     {
+        private string _titolo;
+        private string _autore;
+        private int _durata;
+
+        #region set/get
+
+        public string Titolo
+        {
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Titolo inserito non valido");
+
+                _titolo = value;
+            }
+
+            get{return _titolo;}
+        }
+
+        public string Autore
+        {
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Autore inserito non valido");
+
+                _autore = value;
+            }
+
+            get { return _autore; }
+        }
+
+        public int Durata
+        {
+            private set
+            {
+                if (value < 0)
+                    throw new Exception("Durata inserita non valida");
+
+                _durata = value;
+            }
+
+            get { return _durata; }
+        }
+        #endregion
+
+        public Brano(string titolo, string autore, int durata)
+        {
+            Titolo = titolo;
+            Autore = autore;
+            Durata = durata;
+        }
     }
 }
