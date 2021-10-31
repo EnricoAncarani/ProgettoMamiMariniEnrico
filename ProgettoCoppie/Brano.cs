3a1f4dfca3722321cf1c403ebd21ba69b0542946
Enrico Ancarani <enrico.ancarani.stud@ispascalcomandini.it>
2021-10-31 11:26:15 +0100

    Merge Completato

---
+++ ProgettoCoppie/Brano.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoCoppie
+{
+    class Brano
+    {
+        private string _titolo;
+        private string _autore;
+        private int _durata;
+
+        #region set/get
+
+        public string Titolo
+        {
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Titolo inserito non valido");
+
+                _titolo = value;
+            }
+
+            get { return _titolo; }
+        }
+
+        public string Autore
+        {
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Autore inserito non valido");
+
+                _autore = value;
+            }
+
+            get { return _autore; }
+        }
+
+        public int Durata
+        {
+            private set
+            {
+                if (value < 0)
+                    throw new Exception("Durata inserita non valida");
+
+                _durata = value;
+            }
+
+            get { return _durata; }
+        }
+        #endregion
+
+        public Brano(string titolo, string autore, int durata)
+        {
+            Titolo = titolo;
+            Autore = autore;
+            Durata = durata;
+        }
+
+        public bool shortSong(int durata)
+        {
+            if (_durata < durata) { return true; }
+            else { return false; }
+        }
+
+        public override string ToString()
+        {
+            return "Titolo: '" + _titolo + "' Autore: " + _autore + " Durata : " + _durata + " secondi";
+        }
+    }
+}
--- ProgettoCoppie/MainWindow.xaml.cs
+++ ProgettoCoppie/MainWindow.xaml.cs
@@ -20,9 +20,84 @@ namespace ProgettoCoppie
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<CD> ListaDeiCD;
         public MainWindow()
         {
             InitializeComponent();
+            ListaDeiCD = new List<CD>();
+        }
+
+        private void AggiungiBrano_Click(object sender, RoutedEventArgs e)
+        {
+            string titolo;
+            string autore;
+            int durata;
+            if (string.IsNullOrEmpty(TXTautore.Text) == false)
+            {
+                if (string.IsNullOrEmpty(TXTtitolo.Text) == false)
+                {
+                    try
+                    {
+                        durata = int.Parse(TXTdurata.Text);
+                        titolo = TXTtitolo.Text;
+                        autore = TXTautore.Text;
+                        Brano b = new Brano(titolo, autore, durata);
+                        ListaDeiCD[ComboCD.SelectedIndex].addBrano(b);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Creazione non riuscita");
+                    }
+                }
+            }
+            AggiornaComboBox();
+        }
+
+        private void CreaCD_Click(object sender, RoutedEventArgs e)
+        {
+            string titolo="";
+            string autore="";
+            if (string.IsNullOrEmpty(TXTautore.Text) == false)
+            {
+                if (string.IsNullOrEmpty(TXTtitolo.Text) == false)
+                {
+                    titolo = TXTtitolo.Text;
+                    autore = TXTautore.Text;
+                    CD Prova = new CD(titolo, autore);
+                    ListaDeiCD.Add(Prova);
+                    AggiornaComboBox();
+                }
+            }
+        }
+        public void AggiornaComboBox()
+        {
+            ComboCD.ItemsSource = "";
+            ComboCD.ItemsSource = ListaDeiCD;
+        }
+
+        private void ComboCD_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboCD.SelectedIndex != -1)
+            {
+                AggiungiBrano.IsEnabled = true;
+                RaccogliInfo.IsEnabled = true;
+            }
+            else
+            {
+                AggiungiBrano.IsEnabled = false;
+                RaccogliInfo.IsEnabled = false;
+            }
+        }
+
+        private void RaccogliInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Titolo CD:"+ListaDeiCD[ComboCD.SelectedIndex].Titolo+" "+"Autore CD:"+ ListaDeiCD[ComboCD.SelectedIndex].Autore);
+            List<Brano> listaBrani = ListaDeiCD[ComboCD.SelectedIndex].Brani;
+            MessageBox.Show("Brani:");
+            foreach (Brano b in listaBrani)
+            {
+                MessageBox.Show("Titolo:" + b.Titolo + " " + "Autore:" + b.Autore + " " + "Durata:" + b.Durata.ToString());
+            }
         }
     }
 }
